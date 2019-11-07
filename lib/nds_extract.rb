e24968a87f18b3d84c4b1e993f540f4a6c8d5aac
Stanley Lee <sachih@live.com>
2019-11-07 05:32:51 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,10 +10,10 @@ result = {}
 counter = 0
 while nds[counter] do
 
-  result[nds[counter][:name]] = gross_for_director[nds[counter]]
+  result[nds[counter][:name]] = gross_for_director(nds[counter])
   counter +=1
  end
-
+ 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
